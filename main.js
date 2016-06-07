6e63ac3152ff986cf9e541514a1181454c9d70cc
charlestati <git@charlestati.com>
2016-06-08 01:16:10 +0200

    Setup environment and ESLint

---
+++ main.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const electron = require('electron');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const debug = /--debug/.test(process.argv[2]);
+
+let mainWindow;
+
+function initialize() {
+  function createWindow() {
+    mainWindow = new BrowserWindow({
+      width: 1200,
+      height: 750,
+    });
+
+    mainWindow.loadURL(path.join('file://', __dirname, '/index.html'));
+
+    if (debug) {
+      mainWindow.webContents.openDevTools();
+      mainWindow.maximize();
+    }
+
+    mainWindow.on('closed', () => {
+      mainWindow = null;
+    });
+  }
+
+  app.on('ready', createWindow);
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+
+  app.on('activate', () => {
+    if (mainWindow === null) {
+      createWindow();
+    }
+  });
+}
+
+initialize();
